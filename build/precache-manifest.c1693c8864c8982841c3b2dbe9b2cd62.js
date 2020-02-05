a578cc2edfdd1f0bef1192a04a25ca456214383e
Yuan Yang <yuan.yang@shopee.com>
2020-02-05 23:20:28 +0800

    feat: build

---
+++ build/precache-manifest.c1693c8864c8982841c3b2dbe9b2cd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680e42746ca19f92aeaff930f2541292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901b2cc50d4793b181b7",
+    "url": "/static/css/2.509522b7.chunk.css"
+  },
+  {
+    "revision": "7806c97d4ffb76593b6c",
+    "url": "/static/css/main.7dce2a44.chunk.css"
+  },
+  {
+    "revision": "901b2cc50d4793b181b7",
+    "url": "/static/js/2.c2e179db.chunk.js"
+  },
+  {
+    "revision": "0ff18f7cc35db953a2f667044f6a6ace",
+    "url": "/static/js/2.c2e179db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7806c97d4ffb76593b6c",
+    "url": "/static/js/main.ebe960e6.chunk.js"
+  },
+  {
+    "revision": "db495418320cb7483e47",
+    "url": "/static/js/runtime-main.63ae7275.js"
+  }
+]);
